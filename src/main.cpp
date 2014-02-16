c45cb5ceb9a08a3d5ab100e80f7fb03894e71444
Denis Dydychkin <nyrl@mail.ru>
2014-02-16 13:00:11 +0400

    Finished coilgun code, to be tested now

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 
 #include <unistd.h>
 #include <fcntl.h>
+#include <getopt.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
@@ -294,26 +295,91 @@ class TrikCoilGun
 
 
 
-#if 0
-
-
-static int s_chargeLevel = 0;
-static int s_chargeDurationMs = 0;
-static int s_fireDurationMs = 10;
-static int s_dischargeDelayMs = 100;
-static int s_dischargeDurationMs = 0;
-static int s_dischargeLevel = 0;
-
-
-#endif
-
-
-int main()
+int printUsageHelp()
 {
-  TrikCoilGun coilGun(0,0,0,0,0,0);
+#warning TODO
+  return 1;
+}
 
-  coilGun.charge(1000, 0);
-  coilGun.fire(10, 10, 100);
-  coilGun.discharge(1000, 0);
+int main(int _argc, char* const _argv[])
+{
+  static const char* s_optstring = "h";
+  static const struct option s_longopts[] = {
+    { "help",				no_argument,		NULL,	0},
+    { "msp-i2c-bus",			required_argument,	NULL,	0}, // 1
+    { "msp-i2c-device",			required_argument,	NULL,	0},
+    { "msp-i2c-charge-level",		required_argument,	NULL,	0},
+    { "msp-i2c-discharge-current",	required_argument,	NULL,	0},
+    { "gpio-charge",			required_argument,	NULL,	0}, // 5
+    { "gpio-discharge",			required_argument,	NULL,	0},
+    { "charge-duration",		required_argument,	NULL,	0}, // 7
+    { "charge-level",			required_argument,	NULL,	0},
+    { "fire-predelay",			required_argument,	NULL,	0}, // 9
+    { "fire-duration",			required_argument,	NULL,	0},
+    { "fire-postdelay",			required_argument,	NULL,	0},
+    { "discharge-duration",		required_argument,	NULL,	0}, // 12
+    { "discharge-level",		required_argument,	NULL,	0},
+    { NULL,				0,			NULL,	0},
+  };
+
+  int longopt;
+  int opt;
+
+  unsigned mspI2cBusId = 0x2;
+  unsigned mspI2cDeviceId = 0x48;
+  unsigned mspChargeLevelCmd = 0;
+  unsigned mspDischargeCurrentCmd = 0;
+  unsigned gpioChargeCtrl = 0;
+  unsigned gpioDischargeCtrl = 0;
+
+  unsigned chargeDurationMs = 0;
+  unsigned chargeLevel = 0x10;
+  unsigned firePreDelayMs = 10;
+  unsigned fireDurationMs = 10;
+  unsigned firePostDelayMs = 100;
+  unsigned dischargeDurationMs = 0;
+  unsigned dischargeLevel = 0;
+
+  while ((opt = getopt_long(_argc, _argv, s_optstring, s_longopts, &longopt)) != -1)
+  {
+    switch (opt)
+    {
+      case 'h':		return printUsageHelp();
+
+      case 0:
+        switch (longopt)
+        {
+          case 0:	return printUsageHelp();
+
+          case 1:	mspI2cBusId		= atoi(optarg);	break;
+          case 2:	mspI2cDeviceId		= atoi(optarg);	break;
+          case 3:	mspChargeLevelCmd	= atoi(optarg);	break;
+          case 4:	mspDischargeCurrentCmd	= atoi(optarg);	break;
+          case 5:	gpioChargeCtrl		= atoi(optarg);	break;
+          case 6:	gpioDischargeCtrl	= atoi(optarg);	break;
+
+          case 7:	chargeDurationMs	= atoi(optarg);	break;
+          case 8:	chargeLevel		= atoi(optarg);	break;
+          case 9:	firePreDelayMs		= atoi(optarg);	break;
+          case 10:	fireDurationMs		= atoi(optarg);	break;
+          case 11:	firePostDelayMs		= atoi(optarg);	break;
+          case 12:	dischargeDurationMs	= atoi(optarg);	break;
+          case 13:	dischargeLevel		= atoi(optarg);	break;
+          default:	return printUsageHelp();
+        }
+        break;
+      default:		return printUsageHelp();
+    }
+  }
+
+  cout << "Charge duration " << chargeDurationMs << "ms, level " << chargeLevel << endl;
+  cout << "Fire pre-delay " << firePreDelayMs << "ms, duration " << fireDurationMs << "ms, post-delay " << firePostDelayMs << "ms" << endl;
+  cout << "Discharge duration " << dischargeDurationMs << "ms, level " << dischargeLevel << endl;
+
+  TrikCoilGun coilGun(mspI2cBusId, mspI2cDeviceId, mspChargeLevelCmd, mspDischargeCurrentCmd, gpioChargeCtrl, gpioDischargeCtrl);
+
+  coilGun.charge(chargeDurationMs, chargeLevel);
+  coilGun.fire(firePreDelayMs, fireDurationMs, firePostDelayMs);
+  coilGun.discharge(dischargeDurationMs, dischargeLevel);
 }
 
